400bab6cfd4154d6e39e2e2afb6a1813ea7342fe
Thiago Amorim <thiagobragaamorim@gmail.com>
2024-05-17 23:31:54 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,192 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+let cart = [];
+
+
+//Abre o modal 
+cartBtn.addEventListener("click", function(){
+    updateCartModal();
+    cartModal.style.display = "flex"
+})
+
+//Fecha o modal quando clica fora
+cartModal.addEventListener("click",function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click",function(){
+    cartModal.style.display = "none"
+})
+
+
+menu.addEventListener("click",function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn")
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        addToCart(name,price)
+    }
+})
+
+//Função para adicionar no carrinho
+function addToCart(name, price){
+    const existingItem = cart.find(item => item.name === name)
+        if(existingItem){
+            existingItem.quantity += 1;
+            return;
+        }else{
+            cart.push({
+                name,
+                price,
+                quantity : 1,
+            })
+        }
+    updateCartModal()
+}
+
+//Atualiza o carrinho
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+    
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML = `
+            <div class="flex items-center justify-between">
+                <div>
+                    <p class="font-medium">${item.name}</p>
+                    <p>Qtd: ${item.quantity}</p>
+                    <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+                </div>
+
+    
+                <button class="remove-from-cart-btn" data-name="${item.name}">
+                    Remover
+                </button>
+            </div>
+        `
+        total += item.price * item.quantity;
+        
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+    
+    cartTotal.textContent = total.toLocaleString("pt-BR",{
+        style : "currency",
+        currency : "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+//Function para remover o item do carrinho
+cartItemsContainer.addEventListener("click",function(event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+        
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index,1);
+        updateCartModal();
+    }
+}
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+    
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+
+})
+
+//Finalizar pedido
+checkoutBtn.addEventListener("click",function(){
+    
+    const isOpen = checkRestaurantOpen();
+        if(!isOpen){
+            Toastify({
+                text: "Ops o restaurante está fechado no momento! Horário de funcionamento: 18:00 ás 22:00hrs.",
+                duration: 3000,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "#ef4444",
+                },
+            }).showToast();
+            return;
+    }
+
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressInput.classList.add("border-red-500")
+        addressWarn.classList.remove("hidden")
+        return;
+    }
+
+    //Enviar pro wpp
+    const cartItems = cart.map((item)=>{
+        return(
+            `${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} | `
+        )
+    }).join("")
+    const message = encodeURIComponent(cartItems)
+    const phone = "31993760749"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`,"blank")
+    
+    cart.length = 0;
+    updateCartModal();
+
+})
+
+//Verifica se o restaurante ta aberto
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+    //true = restaurante aberto
+
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600");
+}else{
+    spanItem.classList.remove("bg-green-500");
+    spanItem.classList.add("bg-red-500");
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    fontFamily:{
+      'sans': ['Roboto','sans-serif']
+    },
+    extend: {
+      backgroundImage:{
+        "home": "url('/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
